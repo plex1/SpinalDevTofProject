76cac8cf6d4608d65f5704545b9127ec55735e87
Felix Arnold <felix.arnold@gmail.com>
2019-06-10 21:15:13 +0000

    added TriggerFinder simulation

---
+++ fw/hdl/src/test/scala/TriggerFinderSim.scala
@@ -0,0 +1,44 @@
+package tofperipheral
+
+import spinal.sim._
+import spinal.core._
+import spinal.core.sim._
+
+object TrigFinderTests {
+  def main(args: Array[String]): Unit = {
+
+    val numTaps = 32
+    SimConfig.compile(new TriggerFinder(numTaps)).doSim{ dut =>
+      dut.io.edge #= true
+      dut.io.pattern #= BigInt("10101010101010101010101010101011", 2)
+      sleep(1)
+      println(dut.io.trigPosition.toInt)
+      println(dut.io.trigFound.toBoolean)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 1)
+      dut.io.edge #= false
+      dut.io.pattern #= BigInt("01010101010101010101010101010100" , 2)
+      sleep(1)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 1)
+      dut.io.pattern #= BigInt("01010101010101010101010101010110", 2)
+      sleep(1)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 2)
+      dut.io.pattern #= BigInt("01010101010101010101010101010010", 2)
+      sleep(1)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 3)
+      dut.io.pattern #= BigInt("01010101010100101010101010101010", 2)
+      sleep(1)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 19)
+      dut.io.pattern #= BigInt("01010101010110101010101010101010", 2)
+      sleep(1)
+      assert(dut.io.trigFound.toBoolean == true)
+      assert(dut.io.trigPosition.toInt == 20)
+
+    }
+  }
+}
+
